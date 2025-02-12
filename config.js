7fd4779751d6876c39ae60f5d362cd10aa46cc18
Yutsara4max <168951295+Yutsara4max@users.noreply.github.com>
2025-02-12 15:11:37 +0530

    Update config.js

--- config.js
+++ config.js
@@ -12,7 +12,7 @@ FOOTER: process.env.FOOTER || "Didula MD V2 💚",
 AUTO_STATUS_REPLY: process.env.AUTO_STATUS_REPLY || "true",
 IMAGE_LIMIT: process.env.IMAGE_LIMIT || "3",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://i.ibb.co/tC37Q7B/20241220-122443.jpg",
-SESSION_ID: process.env.SESSION_ID || "F3di0IKZ#Bh708hrCkNoEaYPuzF3BohhXO6JZH5ytWnvo-qMJV3Y",
+SESSION_ID: process.env.SESSION_ID || "BaE1VYBZ#k8cjsVRmwbrHqE7TwE6H8fEFkP8pKyL3ksy-91JSCs0",
 OWNER_NUMBER: process.env.SESSION_ID || "94720244981",
 PREFIX: process.env.PREFIX || "."
 };
