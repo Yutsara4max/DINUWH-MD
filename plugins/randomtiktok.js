12e016d5d13572a38dd507eec0f9457cea5087c1
Yutsara4max <168951295+Yutsara4max@users.noreply.github.com>
2025-02-13 15:44:43 +0530

    Update randomtiktok.js

--- plugins/randomtiktok.js
+++ plugins/randomtiktok.js
@@ -1,13 +1,4 @@
-/*
 
-*_𝗥𝗔𝗡𝗗𝗢𝗠 𝗧𝗜𝗞𝗧𝗢𝗞 𝗗𝗢𝗪𝗡𝗟𝗢𝗔𝗗 𝗡𝗨𝗠𝗕𝗘𝗥 𝗥𝗘𝗣𝗟𝗬 𝗣𝗟𝗨𝗚𝗜𝗡...👇❤_*
-
-*𝚄𝚂𝙰𝙶𝙴 -: .𝚛𝚝𝚒𝚔 𝚜𝚛𝚒 𝚕𝚊𝚗𝚔𝚊*
-*/
-/*
-Please Give Credit 🙂❤️
-⚖️𝐏𝐨𝐰𝐞𝐫𝐞𝐝 𝐁𝐲 - : ©𝐌𝐑 𝐌𝐀𝐍𝐔𝐋 𝐎𝐅𝐂 💚
-*/
 //=============================================
 const { cmd, commands } = require('../command');
 const { fetchJson } = require('../lib/functions');
@@ -16,7 +7,7 @@ const domain = `https://mr-manul-ofc-apis.vercel.app`;
 //=============================================
 cmd({
     pattern: "rtiktok",
-    alias: ["randomtiktok","randomtik","rtik"],
+    alias: ["randomtiktok","randomtik","rtt"],
     desc: 'Download tiktok random Video',
     use: '.rtik Title',
     react: "🎬",
@@ -25,7 +16,7 @@ cmd({
 },
 async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply }) => {
     try {
-        if (!q) return reply('Please provide a title.');
+        if (!q) return reply('*ඔයා හොයන වර්ගෙ වීඩියෝ එකක 𝚝𝚒𝚝𝚕𝚎 එකක් දෙන්න*');
         const response = await fetchJson(`${domain}/random-tiktok?apikey=Manul-Official-Key-3467&query=${q}`);
         const manul = response.data
         const title = manul.title
@@ -34,17 +25,18 @@ async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sen
         const watermark = manul.watermark
         const music = manul.music
         let desc = `
-*🎬 𝐓𝐈𝐊𝐓𝐎𝐊 𝐃𝐎𝐖𝐍𝐋𝐎𝐀𝐃𝐄𝐑 🎬*
+*🎬𝚁𝙰𝙽𝙳𝙾𝙼 𝚃𝙸𝙺𝚃𝙾𝙺 𝙳𝙾𝚆𝙽𝙻𝙾𝙰𝙳𝙴𝚁🎬*
+* *𝙿𝙾𝚆𝙴𝚁𝙳 𝙱𝚈 𝙳𝙸𝙽𝚄𝚆𝙷 𝙼𝙳 🫸🙂‍↔️*
 
 *𝗧𝗶𝘁𝗹𝗲 -:* _~${title}~_
 
-*◄❪ Reply This Message With Nambars ❫►*
+*◄❪ඕනි නම්බර් එක රිප්ලයි කරන්න❫►*
 
-1. 𝗪𝗶𝘁𝗵 𝗪𝗮𝘁𝗲𝗿 𝗠𝗮𝗿𝗸 ✅
-2. 𝗡𝗼 𝗪𝗮𝘁𝗲𝗿 𝗠𝗮𝗿𝗸 ❎
-3. 𝗔𝗨𝗗𝗜𝗢 🎧
+* 1. 𝚆𝙸𝚃𝙷 𝚆𝙰𝚃𝙴𝚁 𝚖𝚊𝚛𝚔 𝚟𝚒𝚍𝚎𝚘 ✅
+* 2. 𝙽𝙾 𝚆𝙰𝚃𝙴𝚁 𝚖𝚊𝚛𝚔 𝚟𝚒𝚍𝚎𝚘 ❎
+* 3. 𝙰𝚄𝙳𝙸𝙾 𝚃𝚈𝙿𝙴🎧
 
-> *⚖️𝐏𝐨𝐰𝐞𝐫𝐞𝐝 𝐁𝐲 - : ©𝐌𝐑 𝐌𝐀𝐍𝐔𝐋 𝐎𝐅𝐂 💚*
+> *⚖️𝙿𝙾𝚆𝙴𝚁𝙳 𝙱𝚈- 𝙳𝙸𝙽𝚄𝚆𝙷 𝙼𝙳 🫣🩷*
 `;
 
         const vv = await conn.sendMessage(from, { image: { url: cover }, caption: desc }, { quoted: mek });
@@ -58,20 +50,20 @@ async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sen
             if (msg.message.extendedTextMessage.contextInfo && msg.message.extendedTextMessage.contextInfo.stanzaId === vv.key.id) {
                 switch (selectedOption) {
                     case '1':
-                    await conn.sendMessage(from,{video:{url: watermark },mimetype:"video/mp4",caption :"> ⚖️𝐏𝐨𝐰𝐞𝐫𝐞𝐝 𝐁𝐲 - : ©𝐌𝐑 𝐌𝐀𝐍𝐔𝐋 𝐎𝐅𝐂 💚"},{quoted:mek})
+                    await conn.sendMessage(from,{video:{url: watermark },mimetype:"video/mp4",caption :"> > *⚖️𝙿𝙾𝚆𝙴𝚁𝙳 𝙱𝚈- 𝙳𝙸𝙽𝚄𝚆𝙷 𝙼𝙳 🫣🩷*"},{quoted:mek})
                         break;
                         
                     case '2':
-                    await conn.sendMessage(from,{video:{url: no_watermark },mimetype:"video/mp4",caption :"> ⚖️𝐏𝐨𝐰𝐞𝐫𝐞𝐝 𝐁𝐲 - : ©𝐌𝐑 𝐌𝐀𝐍𝐔𝐋 𝐎𝐅𝐂 💚"},{quoted:mek})
+                    await conn.sendMessage(from,{video:{url: no_watermark },mimetype:"video/mp4",caption :"> > *⚖️𝙿𝙾𝚆𝙴𝚁𝙳 𝙱𝚈- 𝙳𝙸𝙽𝚄𝚆𝙷 𝙼𝙳 🫣🩷*"},{quoted:mek})
                         break;
        
                     case '3':               
 //============Send Audio======================
-await conn.sendMessage(from,{audio:{url: music },mimetype:"audio/mpeg",caption :"> *⚖️𝐏𝐨𝐰𝐞𝐫𝐞𝐝 𝐁𝐲 - : ©𝐌𝐑 𝐌𝐀𝐍𝐔𝐋 𝐎𝐅𝐂 💚*"},{quoted:mek})
+await conn.sendMessage(from,{audio:{url: music },mimetype:"audio/mpeg",caption :"> > *⚖️𝙿𝙾𝚆𝙴𝚁𝙳 𝙱𝚈- 𝙳𝙸𝙽𝚄𝚆𝙷 𝙼𝙳 🫣🩷*"},{quoted:mek})
                         break;
  
                     default:
-                        reply("Invalid option. Please select a valid option 💗");
+                        reply("*1-3 අතර අන්කයක් රිප්ලයි කරහම්🙂👍*");
                 }
 
             }
@@ -80,7 +72,6 @@ await conn.sendMessage(from,{audio:{url: music },mimetype:"audio/mpeg",caption :
     } catch (e) {
         console.error(e);
         await conn.sendMessage(from, { react: { text: '❌', key: mek.key } })
-        reply('An error occurred while processing your request.');
+        reply('An error occurred while processing your request. *සින්හලෙන් කියන්න ඕනි නෑනෙ එරර් එකක්😞✌️*');
     }
 });
-//=============©𝐌𝐑 𝐌𝐀𝐍𝐔𝐋 𝐎𝐅𝐂 💚==========
