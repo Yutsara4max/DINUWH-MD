69d2a202a8f8afdcb574294cef2925303f0e859b
Yutsara4max <168951295+Yutsara4max@users.noreply.github.com>
2025-02-15 22:10:25 +0530

    Create gemini.js

---
+++ plugins/gemini.js
@@ -0,0 +1,43 @@
+/*
+CREDIT ⦂▹ MR SUPUN FERNANDO
+CREDIT ⦂▹ DARK SHADOW MODZ
+CHANNEL ⦂▹ https://whatsapp.com/channel/0029VaXRYlrKwqSMF7Tswi38
+
+Don't Remove Credit😒💔
+
+**/
+
+const fs=require('fs');
+const path=require('path');
+const config = require('../config');
+const { fetchJson } = require('../lib/functions');
+const { cmd, commands } = require('../command');
+
+//Language 
+var desct = "It Search On Chatgpt Ai For What You Provided."
+var needus = "*Please Give Me Words To Search On AI !*" 
+var cantf  = "*Server Is Busy. Try Again Later.!*"
+
+//================ GEMINI ================
+
+cmd({
+    pattern: "gemini",
+    alias: ["ai5","gpt5","openai5","chat5"],
+    react: '👾',
+    desc: desct,
+    category: "ai",
+    use: '.gemini <query>',
+    filename: __filename
+},
+async(conn, mek, m,{from, l, prefix, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
+try{
+if(!q) return reply(needus)
+//let res = (await fetchJson('https://hercai.onrender.com/v3/hercai?question=' + q)).response
+let res = await fetchJson('https://hercai.onrender.com/gemini/hercai?question='+q)
+
+return await reply(res.reply)
+} catch (e) {
+reply(cantf)
+console.log(e)
+}
+})
