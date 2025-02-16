3d2b8897a11443d77443ac4e6ebc11bc5d3c37e1
Yutsara4max <168951295+Yutsara4max@users.noreply.github.com>
2025-02-16 17:53:43 +0530

    Update index.js

--- index.js
+++ index.js
@@ -49,14 +49,6 @@ const port = process.env.PORT || 8000;
 
 //=============================================
 
-async function connectToWA() {
-//===========connect mongodb===================
-const connectDB = require('./lib/mongodb')
-connectDB();
-//==============================================
-const {readEnv} = require('./lib/database')   
-const config = await readEnv();
-//==============================================
         
 console.log("Connecting Queen Meri Md bot 🧬...");
 const { state, saveCreds } = await useMultiFileAuthState(__dirname + '/auth_info_baileys/')
